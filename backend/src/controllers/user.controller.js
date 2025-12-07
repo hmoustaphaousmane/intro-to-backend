cbc205a5905e77cf2fe988eeef9f7f54b13c9567
hmoustaphaousmane <hmoustaphaousmane01@gmail.com>
2025-12-07 15:16:59 +0100

    feat: add user login route
    - user login route and controller
    - add pre hook to hash user's password before saving
    - add compare password method to use schema

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -1,4 +1,5 @@
 import { User } from "../models/user.model.js";
+import bcrypt from "bcrypt";
 
 const registerUser = async (req, res, next) => {
   try {
@@ -14,6 +15,9 @@ const registerUser = async (req, res, next) => {
     if (existing) {
       return res.status(400).json({ message: "User already exists!" });
     }
+
+    // hash the password
+    // const hash = bcrypt.hashSync(password, 10);
 
     // create user
     const user = await User.create({
@@ -35,4 +39,34 @@ const registerUser = async (req, res, next) => {
   }
 };
 
-export { registerUser };
+const loginUser = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+
+    // check if user exists
+    const user = await User.findOne({ email: email.toLowerCase() });
+
+    if (!user) return res.status(400).json({ message: "User not found." });
+
+    // check if password is correct
+    const isMatch = await user.comparePassword(password);
+    if (!isMatch)
+      return res.status(400).json({ message: "Invalid credentials." });
+
+    res.status(200).json({
+      message: "User logged in.",
+      user: {
+        id: user._id,
+        email: user.email,
+        username: user.username,
+      },
+    });
+  } catch (error) {
+    console.log(error);
+    res
+      .status(500)
+      .json({ message: "Internal server error!", error: error.message });
+  }
+};
+
+export { registerUser, loginUser };
