f7299d72c559d21f6eb7d06952381040f3734e04
hmoustaphaousmane <hmoustaphaousmane01@gmail.com>
2025-12-07 14:08:12 +0100

    feat: add regiter user route and controller

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,14 @@
 import express from "express";
 
+// routes imports
+import userRouter from "./routes/user.route.js";
+
 const app = express(); // create an express app
+
+// Middlewares
+app.use(express.json());
+
+// routes declaration
+app.use("/api/v1/users", userRouter);
 
 export default app;
